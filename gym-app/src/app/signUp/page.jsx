e8825adf908dff1c0948ed711fb7bd981834715b
unknown <salomoneyd@gmail.com>
2023-06-14 16:21:54 -0500

    Fixing CSS propertys of login and SingUp visually, also redirecting pages after login or Sign Up are successfull

--- gym-app/src/app/signUp/page.jsx
+++ gym-app/src/app/signUp/page.jsx
@@ -1,43 +1,50 @@
-"use client"
-
+"use client";
+import "./signUp.css";
 
 const SignUp = () => {
-    const API_URL = process.env.API_KEY;
+  const API_URL = process.env.API_KEY;
 
-    async function sendUser(userData) {
-        const res = await fetch(`${API_URL}/api/users`, {
-          method: 'POST',
-          headers: new Headers({ 'Content-Type': 'application/json' }),
-          body: JSON.stringify(userData)
-        });
-        if (res.ok) return res.json();
-        throw new Error('Email already taken!');
-      }
+  async function sendUser(userData) {
+    const res = await fetch(`${API_URL}/api/users`, {
+      method: "POST",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      body: JSON.stringify(userData),
+    });
+    if (res.ok) {
+      alert("Sign Up Successful")
+      window.location.href="/login";
+      return res.json()}
+    throw new Error("Email already taken!");
+  }
 
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-        const { userName, email, password } = e.target;
-        const userData = (
-            {userName: userName.value.toLowerCase(),
-        email: email.value.toLowerCase(),
-        password: password.value}
-        );
-        console.log(userData)
-        sendUser(userData)
-      };
-  return(
-  <main>
-    <form onSubmit={handleSubmit}>
-        <input type="text" name="userName" required/>
-        <input type="email" name="email" required/>
-        <input type="password" name="password" required/>
-        <button type="submit" >
-            Sign Up →
-          </button>
-    </form>
-  </main>)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { userName, email, password } = e.target;
+    const userData = {
+      userName: userName.value.toLowerCase(),
+      email: email.value.toLowerCase(),
+      password: password.value,
+    };
+    sendUser(userData);
   };
-  
-  export default SignUp;
-  
+  return (
+    <form className='signUp--container' onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor='userName'> user name </label>
+        <input type='text' name='userName' id="userName" required />
+      </div>
+      <div>
+        <label htmlFor='email'>email </label>
+        <input type='email' name='email' id="email" required />
+      </div>
+      <div>
+        <label htmlFor='password'>password </label>
+        <input type='password' name='password' id="password" required />
+      </div>
+      <button type='submit'>Sign Up →</button>
+    </form>
+  );
+};
+
+export default SignUp;
