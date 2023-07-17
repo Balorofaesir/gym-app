3e1e4276fb3017fac1f88d131c221f667fd9cae1
unknown <salomoneyd@gmail.com>
2023-07-17 16:49:06 -0500

    for vercel preview

--- gym-app/next.config.js
+++ gym-app/next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 require('dotenv').config();
 
-const nextConfig = {
-    env: {
-        API_KEY: 'http://localhost:8080',
-      },  
-      css: ['./src/styles/globals.css'],
-}
-
-module.exports = nextConfig
 // const nextConfig = {
-//   env: {
-//     API_KEY: process.env.API_KEY,
-//   },
-//   css: ['./src/styles/globals.css'],
-// };
-// console.log("prosss", process.env.API_KEY)
-// module.exports = nextConfig;
+//     env: {
+//         API_KEY: 'http://localhost:8080',
+//       },  
+//       css: ['./src/styles/globals.css'],
+// }
+
+// module.exports = nextConfig
+const nextConfig = {
+  env: {
+    API_KEY: process.env.API_KEY,
+  },
+  css: ['./src/styles/globals.css'],
+};
+console.log("prosss", process.env.API_KEY)
+module.exports = nextConfig;
